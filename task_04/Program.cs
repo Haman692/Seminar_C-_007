d6b98599033ee002e84773f4639f83e0cccf207c
Haman692 <haman692@yandex.ru>
2022-12-13 02:10:29 +0700

    task048

---
+++ task_04/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+
+
+int AddNumber(string argument)
+{
+    System.Console.WriteLine(argument);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+void MatrixMplusN(int[,] arr, int leftRange, int rightRange)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = i + j;
+        }
+    }
+
+}
+
+void ShowMatrix(int [,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write(arr[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int colums = AddNumber("Enter the number of columns");
+int lines = AddNumber("Enter the number of lines");
+
+int [,] matrix = new int[colums, lines];
+MatrixMplusN(matrix, min, max);
+ShowMatrix(matrix);
